fb019fc493dfecad6fff04536de58e91dd9a0c31
AnhTran07101998 <60461156+AnhTran07101998@users.noreply.github.com>
2020-04-19 16:16:58 -0600

    Complex.h

---
+++ Complex.h
@@ -0,0 +1,37 @@
+#include <iostream>
+
+class Complex {
+	private:
+		double _real;
+		double _imag;
+
+	public:
+		Complex ();
+		Complex (double &real);
+		Complex (double real, double imag);
+		Complex (const Complex &value);
+		~Complex ();
+
+		double RealNumberGen() const;
+		double ImagNumberGen() const;
+
+		void print();
+
+		Complex add(Complex);
+		Complex sub(Complex);
+		Complex mult(Complex);
+		Complex div(Complex);
+
+		double magnitude();
+		double phase();
+
+		Complex operator+ (Complex value);
+		Complex operator- (Complex value);
+		Complex operator* (Complex value);
+		Complex operator/ (Complex value);
+		Complex operator= (Complex value);
+
+		std::ostream& operator << (std::ostream &out, const Complex &c1);
+		std::istream& operator >> (std::istream &in, Complex &c1);
+
+};
